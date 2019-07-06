cc8579d019ec93ea01357fa0892a38854b837fb9
mzambia70 <mzambia70@gmail.com>
2019-07-06 22:40:19 +0300

    added my projects application

---
+++ hood/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import *
+
+admin.site.register(Neighbourhood)
+admin.site.register(User)
+admin.site.register(Business)
+admin.site.register(Category)
+admin.site.register(News)
+admin.site.register(Service)
