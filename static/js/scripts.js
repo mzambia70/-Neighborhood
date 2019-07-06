2948c8b4a73cdd1ff4d77d43e1c7076d62d08329
mzambia70 <mzambia70@gmail.com>
2019-07-06 22:38:27 +0300

    added my static folder which holds my js,css and project image folders

---
+++ static/js/scripts.js
@@ -0,0 +1,23 @@
+//User Interface logic
+$(document).ready(function () {
+    $(".one").click(function () {
+        $(".one").removeClass("active");
+        $(".one").addClass("active");
+    });
+    $(".two").click(function () {
+        $(".two").removeClass("active");
+        $(".two").addClass("active");
+    });
+    $(".three").click(function () {
+        $(".three").removeClass("active");
+        $(".three").addClass("active");
+    });
+    $(".four").click(function () {
+        $(".four").removeClass("active");
+        $(".four").addClass("active");
+    });
+    $(".five").click(function () {
+        $(".five").removeClass("active");
+        $(".five").addClass("active");
+    });
+});
